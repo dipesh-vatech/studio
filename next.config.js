357ca730aee49b11582c2f7f5d0bcd41358e240f
Dipesh Patel <eng.dipesh.DP@gmail.com>
2025-07-02 16:52:22 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ next.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // These modules are not used in the browser, so we can safely ignore them.
+      config.externals.push('handlebars');
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
