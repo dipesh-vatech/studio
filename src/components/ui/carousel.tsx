5fa467e0e025a5fb6329736d34c281b68b81cebd
Dipesh Patel <eng.dipesh.DP@gmail.com>
2025-07-14 19:07:15 +0000

    according to current code and features which is working fine is there an

--- src/components/ui/carousel.tsx
+++
@@ -1,5 +0,0 @@
-"use client"
-
-// This component is currently unused and was causing build errors.
-// It is kept here for potential future use, but its dependencies have been removed.
-// To use it, you will need to run: npm install embla-carousel-react
